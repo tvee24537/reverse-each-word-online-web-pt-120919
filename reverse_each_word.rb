7c830c967ec17070605742f743fcbf628ce5ce9f
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-20 20:35:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
   sentence = string.split(" ") #turns string of words into array
   words = []  
   sentence.collect {|reword| reword.reverse}
-    words << sentence
-    return words.join(" ")
+    return sentence.join(" ")
+    
 #  sentence.each do |reword|
 #    words << reword.reverse
 #  end
